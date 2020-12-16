7f3b1ffbef78c54d1fbace9a2ed614caefdc4ab9
thibault <thibault.bouzat@essec.edu>
2020-12-16 21:01:53 +0800

    app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import NewTask from './NewTask.js';
+import TaskList from './TaskList.js';
 
-function App() {
+export default () => {
+const [tasks, setTasks] = useState([]);
+const [nextId, setNextId] = useState(0);
+
+const createTask = description => {
+  setTasks(tasks => ([...tasks, {id: nextId, description, done: false}]));
+  setNextId(nextId + 1);
+};
+
+const markTaskDone = id => {
+    const newTaskList = tasks.map(task => {
+      if(task.id === id) {
+        task.done = true;
+      }
+      return task;
+    });
+    setTasks(newTaskList);
+  };
+
+const deleteTask = id => {
+  setTasks(tasks.filter(task => task.id !== id));
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <div className="jumbotron">
+            <h1 className="display-4">Todo App</h1>
+          </div>
+          <NewTask createTask={createTask} />
+            <br />
+          <TaskList tasks={tasks} markTaskDone={markTaskDone} deleteTask={deleteTask}/>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/TaskList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Task from './Task.js';
+
+export default ({ tasks, markTaskDone, deleteTask }) => {
+  return (
+    <ul className="list-group">
+    {tasks.map(task => (
+      <li key={task.id} className="list-group-item">
+        <Task task={task} markTaskDone={markTaskDone} deleteTask={deleteTask}/>
+      </li>
+    ))}
+    </ul>
+  );
+}
